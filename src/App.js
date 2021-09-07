f334db3cb3d4f2b9556b0a90f592d3bedf51722b
Piero Okyere-Cobs <pcobby@ymail.com>
2021-09-07 13:01:29 +0000

    addition of create contact functionality (#2)

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 
 class App extends Component {
-	 state = {
-		  contacts: []
-	 }
+	state = {
+		contacts: [],
+		screen: 'list'
+	}
 
-	 componentDidMount() {
-		  ContactsAPI.getAll()
-				.then(contacts => this.setState({
-					 contacts
-				}))
-	 }
+	componentDidMount() {
+		ContactsAPI.getAll()
+			.then(contacts => this.setState({
+					contacts
+			}))
+	}
 
-	 removeContact = contact => {
-		  this.setState(currentState => ({
-				contacts: currentState.contacts.filter(c => (
-					 c.id !== contact.id
-				))
-		  }))
+	removeContact = contact => {
+		this.setState(currentState => ({
+			contacts: currentState.contacts.filter(c => (
+					c.id !== contact.id
+			))
+		}))
 
-		ContactsAPI.remove(contact)
-	 }
+	ContactsAPI.remove(contact)
+	}
 
-	 render() {
-		  return (
-				<div>
-					 <ListContacts 
-						  contacts={this.state.contacts}
-						  onDeleteContact={this.removeContact}
-					 />
-				</div>
-		  );
-	 }
+	render() {
+		return (
+			<div>
+				{
+					this.state.screen === 'list' && <ListContacts 
+						contacts={this.state.contacts}
+						onDeleteContact={this.removeContact}
+						onNavigate={() => {
+							this.setState({
+								screen: 'create'
+							})
+						}}
+					/>
+				}
+				
+				{ this.state.screen === 'create' && <CreateContact/> }
+			</div>
+		);
+	}
 }
 
 
